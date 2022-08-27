5bfe71c533a315cca2962bca441a3d36de5f11f3
Luca De Menego <lucademenego99@gmail.com>
2022-08-27 11:05:27 +0200

    Add about image

--- src/lib/sections/About.svelte
+++ src/lib/sections/About.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import SectionTitle from "../components/SectionTitle.svelte";
+	import AboutMeImage from "../../assets/about-me.jpg";
 </script>
 
 <div class="about-me" id="about" data-aos="fade-up">
@@ -10,7 +11,7 @@
     <div class="row">
 		<p class="about">Hi, I'm Luca. I'm a 23 years old guy who lives in Laggio di Cadore. Never heard of it? It's a small village amongst the Dolomites in Italy, quite little-known, but it has a lot to offer. I spent my childhood here, went to school, made some friends... the usual life you would expect. <br> I have always been fond of computers, I built my own one when I was a kid, and I started my coding journey during high-school as a self-taught Java Developer. Then I finally went to the University of Trento, bachelor's degree in Computer Science. The path has actually been quite straightforward, and in no time I found a job, I graduated and I started the Master's degree. <br> And here we are. One year's left, the first went pretty well, I'm on vacation and I'm celebrating by creating this Portfolio. Maybe I should take advantage of this period to take a break, but I'm having fun :)</p>
         <img
-            src="https://picsum.photos/500/500"
+            src="{AboutMeImage}"
             alt="Myself"
         />
     </div>
@@ -46,6 +47,8 @@
 	img {
 		filter: opacity(50%) blur(2px) drop-shadow(18px 18px 5px var(--primary-color));
 		transition: filter 0.2s ease-in-out;
+		width: 500px;
+		height: 500px;
 		max-width: 90vw;
 	}
 	img:hover {
